3e2c4c89235f7387ef4f8ac61b217201ff191df9
Viktorya <vika.kokorina.933@gmail.com>
2024-02-26 14:56:35 +0300

    сохранение

--- Seminar_8.py
+++ Seminar_8.py
@@ -4,8 +4,8 @@
 # для изменения и удаления данных.
 
 
-# 1.Создание файла:
-#     - открываем файл на дозапись #a
+# 1.Создание файла:+++
+#     - открываем файл на дозапись #a +++
 # 2.Добавление контакта
 #     - запросить у пользователя новый контакта
 #     - открыть файл на дозапись #a
@@ -22,8 +22,81 @@
 #     - осуществляем поиск контакта
 #     - вывести на экран
 # 5. Создание UI:
-#     - вывести меню на экран
-#     - запросить у пользователя вариант действия
+#     - вывести меню на экран +++
+#     - запросить у пользователя вариант действия +++
 #     - запустить соответствующую функцию 
 #     - осуществить возможность выхода из программы
+
+
+
+def input_surname():
+    return input('Введите фамилию контакта: ')
+
+def input_name():
+    return input('Введите имя контакта: ')
+
+def input_patronymic():
+    return input('Введите отчество контакта: ')
+
+def input_phone():
+    return input('Введите номер контакта: ')
+
+def input_address():
+    return input('Введите город контакта: ')
+
+def create_contact():
+    surname = input_surname()
+    name = input_name()
+    patronymic = input_patronymic()
+    phone = input_phone()
+    address = input_address()
+    return f'{surname} {name} {patronymic}: {phone}\n{address}\n\n'
+    
+
+def add_contact():
+    contact_str = create_contact()
+    with open("phonebook.txt", 'a', encoding='UTF-8') as file:
+        file.write(contact_str)
+
+
+def print_contacts():
+    pass
+
+def search_contacts():
+    pass
+
+
+def interface():
+    with open("phonebook.txt", "a", encoding='UTF-8'):
+        pass
+
+
+var = 0
+while var != '4':
+    print(
+        'Возможные варианты:\n'
+        '1. Добавить контакт\n'
+        '2. Вывести на экран\n'
+        '3. Поиск контакта\n'
+        '4. Выход'
+        )
+    var = input('Выберите вариант действия: ')
+    while var not in ('1', '2', '2', '4'):
+        print('Неккоректный ввод: ')
+        var = input('Выберите вариант действия: ')
+
+    match var:
+        case '1':
+            add_contact()
+        case '2':
+            print_contacts() 
+        case '3':
+            search_contacts()  
+        case '4':
+            print('Пока')
+
+
+
+if __name__ == '_main__':
+    interface()
 
